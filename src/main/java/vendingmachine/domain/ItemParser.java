517eea6f70d41bcebb7a8de76ef6bfcb910f842e
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-01 18:04:19 +0900

    feat : 추가 상품 입력 item및 items 객체 생성 파싱 코드 작성

---
+++ src/main/java/vendingmachine/domain/ItemParser.java
@@ -0,0 +1,26 @@
+package vendingmachine.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemParser {
+
+    public static List<Item> parseProducts(String data) {
+        List<Item> products = new ArrayList<>();
+        String[] productStrings = data.split("];\\[");
+
+        for (String productString : productStrings) {
+            productString = productString.replace("[", "").replace("]", "");
+            String[] attributes = productString.split(",");
+
+            String name = attributes[0];
+            int price = Integer.parseInt(attributes[1]);
+            int quantity = Integer.parseInt(attributes[2]);
+
+            Item product = new Item(name, price, quantity);
+            products.add(product);
+        }
+
+        return products;
+    }
+}
