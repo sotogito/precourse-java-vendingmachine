faeb5e29251a71008769104156549ea7458795e8
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-02 15:55:32 +0900

    chore : 자판기 동전 최소 잔돈 가져오기

--- src/main/java/vendingmachine/domain/cashers/CashChangeCalculator.java
+++ src/main/java/vendingmachine/domain/cashers/CashChangeCalculator.java
@@ -12,24 +12,23 @@ public class CashChangeCalculator {
 
     //}
 
+    //note VendingMachine이 가지고 있는 동전 최소 개수로 반환하기
     public EnumMap<Coin, Integer> calculate(int userCash,EnumMap<Coin, Integer> vendingMachineCash) {
         int amount = userCash;
         EnumMap<Coin, Integer> changeResult = new EnumMap<>(Coin.class);
 
-        // 큰 동전부터 순서대로 처리
         Coin[] coins = Coin.values();
         Arrays.sort(coins, (a, b) -> b.getAmount() - a.getAmount());
 
         for (Coin coin : coins) {
-            int availableCoins = vendingMachineCash.getOrDefault(coin, 0); // 사용 가능한 동전 개수
-            int usedCoins = Math.min(amount / coin.getAmount(), availableCoins); // 사용할 수 있는 동전 개수
+            int availableCoins = vendingMachineCash.getOrDefault(coin, 0); // 자판기가 보유한 해당 동전의 개수
+            int usedCoins = Math.min(amount / coin.getAmount(), availableCoins); // 사용할 수 있는 동전의 개수
 
-            if (usedCoins > 0) {
-                changeResult.put(coin, usedCoins);
-                amount -= usedCoins * coin.getAmount();
+            if (usedCoins > 0) { // 사용할 동전이 있으면
+                changeResult.put(coin, usedCoins); // 잔돈 결과에 해당 동전의 사용 개수를 추가
+                amount -= usedCoins * coin.getAmount(); // 사용한 동전의 금액만큼 amount에서 차감
             }
         }
-
         if (amount > 0) {
             return changeResult;
         }
