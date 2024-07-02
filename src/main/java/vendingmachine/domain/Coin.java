0935a7b8da54e0aa7718ccc3c5af51d251d4425d
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-02 15:55:05 +0900

    refactor : 자판기 동전 랜덤 Map으로 받아오기

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -23,26 +23,30 @@ public enum Coin {
         return amount;
     }
 
+    //note 무작위 coin 생성하기
     public static EnumMap<Coin, Integer> getVendingMachineCoins(int money) {
-        EnumMap<Coin, Integer> coinMap = new EnumMap<>(Coin.class);
-        Coin[] coins = Coin.values();
-        Arrays.sort(coins, (a, b) -> b.getAmount() - a.getAmount()); // 큰 동전부터 정렬
+        EnumMap<Coin, Integer> result = new EnumMap<>(Coin.class);
 
-        for (Coin coin : coins) {
-            int count = money / coin.getAmount();
-            if (count > 0) {
-                coinMap.put(coin, count);
-                money -= count * coin.getAmount();
+        Coin[] coins = Coin.values();
+        Arrays.sort(coins, (a, b) -> b.getAmount() - a.getAmount()); //큰 동전부터 정렬
+
+        while (money > 0) {
+            Coin randomCoin = getRandomCoin(coins); //랜덤의 동전을 가져옴
+            int coinValue = randomCoin.getAmount();
+
+            if (money >= coinValue) {
+                int maxCount = money / coinValue; //최대로 구할 수 있는 개수
+                int count = Randoms.pickNumberInRange(1, maxCount); //최대 개수에서 랜덤으로 가져감
+                result.merge(randomCoin, count, Integer::sum);
+                money -= count * coinValue;
             }
         }
-
-        return coinMap;
+        return result;
     }
 
-    private static Coin getRandomCoin() {
-        Coin[] coinsArray = Coin.values();
-        int randomIndex = Randoms.pickNumberInRange(0, coinsArray.length - 1);
-        return coinsArray[randomIndex];
+    private static Coin getRandomCoin(Coin[] coins) {
+        int randomIndex = Randoms.pickNumberInRange(0, coins.length - 1);
+        return coins[randomIndex];
     }
 
 
