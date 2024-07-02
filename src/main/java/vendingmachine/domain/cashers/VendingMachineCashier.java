e86f2f3229e2df0be87e06d4bcdc58dab57be17c
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-02 18:00:55 +0900

    refactor : 최종 잔돈 반환 전용 클래스 생성

--- src/main/java/vendingmachine/domain/cashers/VendingMachineCashier.java
+++ src/main/java/vendingmachine/domain/cashers/VendingMachineCashier.java
@@ -1,56 +1,37 @@
 package vendingmachine.domain.cashers;
 
 import vendingmachine.domain.Coin;
-import vendingmachine.util.message.ErrorMessage;
+import vendingmachine.domain.cashers.calculators.CashChangeCalculator;
+import vendingmachine.domain.cashers.calculators.ChangeCalculator;
+import vendingmachine.domain.cashers.calculators.VendingMachineCashCalculator;
 import vendingmachine.util.validators.CashValidator;
 import vendingmachine.util.validators.VendingMachineCashValidator;
 
-import java.util.Arrays;
 import java.util.EnumMap;
 
 public class VendingMachineCashier {
 
-    private EnumMap<Coin, Integer> cash;
+    private final EnumMap<Coin, Integer> cash;
+    private ChangeCalculator changeCalculator;
 
     public VendingMachineCashier(int money) {
-
         validate(money);
-        cash = Coin.getVendingMachineCoins(money);
+        changeCalculator = new VendingMachineCashCalculator(); //note 자판기 랜덤 coin 리스트
+        cash = changeCalculator.calculate(money,Coin.getInitMap());
     }
 
-    //TODO 계산하는 로직은 따로 뺴는게 좋을 거 같다.
-    public EnumMap<Coin, Integer> calculateBalance(int userMoney) {
-        int amount = userMoney;
-        EnumMap<Coin, Integer> balance = new EnumMap<>(Coin.class);
-
-        // 큰 동전부터 순서대로 처리
-        Coin[] coins = Coin.values();
-        Arrays.sort(coins, (a, b) -> b.getAmount() - a.getAmount());
-
-        for (Coin coin : coins) {
-            int availableCoins = cash.getOrDefault(coin, 0); // 사용 가능한 동전 개수
-            int usedCoins = Math.min(amount / coin.getAmount(), availableCoins); // 사용할 수 있는 동전 개수
-
-            if (usedCoins > 0) {
-                balance.put(coin, usedCoins);
-                amount -= usedCoins * coin.getAmount();
-            }
-        }
-
-        // 남은 금액이 있으면 예외 발생 (남은 금액을 처리할 수 없는 경우)
-        if (amount > 0) {
-            return cash;
-        }
-
-        // 사용한 동전의 개수를 업데이트
-        for (Coin coin : balance.keySet()) {
-            cash.put(coin, cash.get(coin) - balance.get(coin));
-        }
-
-        return balance;
+    public EnumMap<Coin, Integer> calculateChangeAndUpdateVendingMachineCoin(int userCash) {
+        changeCalculator = new CashChangeCalculator(); //note 사용자 잔액 coin 리스트
+        EnumMap<Coin, Integer> changeCash = changeCalculator.calculate(userCash, cash);
+        updateVendingMachineCoin(changeCash);
+        return changeCash;
     }
 
-
+    private void updateVendingMachineCoin(EnumMap<Coin, Integer> userChangeCash){
+        for (Coin coin : userChangeCash.keySet()) {
+            cash.put(coin, cash.get(coin) - userChangeCash.get(coin));
+        }
+    }
 
     public void validate(int money){
         CashValidator validator = new VendingMachineCashValidator();
