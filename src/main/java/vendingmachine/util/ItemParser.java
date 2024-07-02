9f1a87c0761fc28963e503e3f66bb94043605ff7
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-02 12:57:55 +0900

    feat : 기능 분리하기

--- src/main/java/vendingmachine/util/ItemParser.java
+++ src/main/java/vendingmachine/util/ItemParser.java
@@ -7,22 +7,64 @@ import java.util.List;
 
 public class ItemParser {
 
-    public static List<Item> parseProducts(String data) {
-        List<Item> products = new ArrayList<>();
-        String[] productStrings = data.split("];\\[");
+    public static List<Item> parseProducts(String data){
+        List<Item> results = new ArrayList<>();
 
-        for (String productString : productStrings) {
-            productString = productString.replace("[", "").replace("]", "");
-            String[] attributes = productString.split(",");
+        String[] splitDataBySemicolon = getSplitBySemicolonData(data);
+        String[][] splitData = getSplitData(splitDataBySemicolon);
 
-            String name = attributes[0];
-            int price = Integer.parseInt(attributes[1]);
-            int quantity = Integer.parseInt(attributes[2]);
+        for(String[] itemData : splitData) {
+            results.add(createItem(itemData));
+        }
+        return results;
+    }
 
-            Item product = new Item(name, price, quantity);
-            products.add(product);
+    private static Item createItem(String[] itemData){
+        String name = itemData[0];
+        int price = Integer.parseInt(itemData[1]);
+        int quantity = Integer.parseInt(itemData[2]);
+
+        return new Item(name, price, quantity);
+    }
+
+    private static String[][] getSplitData(String[] splitDataBySemicolon) {
+        String[][] results = new String[splitDataBySemicolon.length][];
+
+        for (int i = 0; i < splitDataBySemicolon.length; i++) {
+            String data = splitDataBySemicolon[i]; //[이름,수량,가격]
+            data = getDeleteBracketsData(data); // 이름,수량,가격
+            String[] attributes = getSplitCommaData(data); //이름 수량 가격
+
+            if (attributes.length != 3) {
+                throw new IllegalArgumentException("[이름, 가격, 수량]대로 3가지 요소를 적어주세요");
+            }
+            results[i] = attributes;
         }
 
-        return products;
+        return results;
+    }
+
+    private static String[] getSplitCommaData(String data){
+        try{
+            return data.split(",");
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException(", 콤마로 이름, 가격, 수량을 구분해주세요.");
+        }
+    }
+
+    private static String getDeleteBracketsData(String data){
+        try{
+            return data.replace("[", "").replace("]", "");
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("대괄호로 [이름,가격, 수량]을 감싸주세요");
+        }
+    }
+
+    private static String[] getSplitBySemicolonData(String data) throws IllegalArgumentException{
+        try{
+            return data.split(";");
+        }catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("세미크론으로 아이템을 구분해주세요.");
+        }
     }
 }
