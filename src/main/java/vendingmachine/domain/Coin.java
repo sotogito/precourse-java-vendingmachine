1fc2ecce2e8dd5ebe7cd395f61ca0e334f66d084
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-02 18:01:35 +0900

    refactor : 자판기 coin 생성 기능 삭제 인터페이스로

--- src/main/java/vendingmachine/domain/Coin.java
+++ src/main/java/vendingmachine/domain/Coin.java
@@ -23,30 +23,19 @@ public enum Coin {
         return amount;
     }
 
-    //note 무작위 coin 생성하기
-    public static EnumMap<Coin, Integer> getVendingMachineCoins(int money) {
-        EnumMap<Coin, Integer> result = new EnumMap<>(Coin.class);
 
-        Coin[] coins = Coin.values();
-        Arrays.sort(coins, (a, b) -> b.getAmount() - a.getAmount()); //큰 동전부터 정렬
-
-        while (money > 0) {
-            Coin randomCoin = getRandomCoin(coins); //랜덤의 동전을 가져옴
-            int coinValue = randomCoin.getAmount();
-
-            if (money >= coinValue) {
-                int maxCount = money / coinValue; //최대로 구할 수 있는 개수
-                int count = Randoms.pickNumberInRange(1, maxCount); //최대 개수에서 랜덤으로 가져감
-                result.merge(randomCoin, count, Integer::sum);
-                money -= count * coinValue;
-            }
-        }
-        return result;
-    }
 
     private static Coin getRandomCoin(Coin[] coins) {
         int randomIndex = Randoms.pickNumberInRange(0, coins.length - 1);
         return coins[randomIndex];
+    }
+
+    public static EnumMap<Coin, Integer> getInitMap() {
+        EnumMap<Coin, Integer> result = new EnumMap<>(Coin.class);
+        for (Coin coin : Coin.values()) {
+            result.put(coin, 0);
+        }
+        return result;
     }
 
 
