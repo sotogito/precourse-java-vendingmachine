4747ca69bf3d700c5161153ba1f3d24457169021
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-04 18:56:38 +0900

    feat : 상품 등록 input String parser 생성

--- src/main/java/vendingmachine/domain/ItemParser.java
+++ src/main/java/vendingmachine/domain/ItemParser.java
@@ -1,4 +1,4 @@
-package vendingmachine.controller;
+package vendingmachine.domain;
 
 import vendingmachine.domain.product.Item;
 import vendingmachine.domain.product.Items;
@@ -7,9 +7,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ItemParser {
+
+    /*
+    public static void main(String[] args) {
+        Items items = parseItems("[,1500,5ㅈ4];[사이다,1000,10]");
+
+        System.out.println(items);
+    }
+
+     */
+
     public static Items parseItems(String input) {
         if (input == null || input.isEmpty()) {
-            throw new IllegalArgumentException("Input string cannot be null or empty");
+            throw new IllegalArgumentException("입력된 상품이 없습니다.");
         }
 
         String[] itemStrings = input.split(";");
@@ -20,7 +30,7 @@ public class ItemParser {
             String[] itemDetails = itemString.split(",");
 
             if (itemDetails.length != 3) {
-                throw new IllegalArgumentException("Each item must have a name, price, and stock");
+                throw new IllegalArgumentException("[상품이름,가격,수량] 순으로 입력하고 ; 로 구분 해주세요.");
             }
 
             try {
@@ -29,7 +39,7 @@ public class ItemParser {
                 int stock = Integer.parseInt(itemDetails[2]);
                 itemList.add(new Item(name, price, stock));
             } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("Price and stock must be integers");
+                throw new IllegalArgumentException("가격과 수량은 숫자로 입력해주세요.");
             }
         }
 
