0bdf29741a5e346bbc21581710b78cde5500e239
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-04 17:27:51 +0900

    feat : 메인 컨트롤러 생성 및 Application 연

--- src/main/java/vendingmachine/Application.java
+++ src/main/java/vendingmachine/Application.java
@@ -1,7 +1,11 @@
 package vendingmachine;
 
+import vendingmachine.controller.MainController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        MainController controller = new MainController();
+        controller.main();
     }
 }
