c02010fa91a520f67c697dfc7c6babe9e6e5a766
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-04 19:27:11 +0900

    feat : 출력물 상수 추가 작성

--- src/main/java/vendingmachine/util/message/ErrorMessage.java
+++ src/main/java/vendingmachine/util/message/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
 
     public final static String INSUFFICIENT_FUNDS_EXCEPTION = "가지고 있는 돈보다 아이템의 가격이 비싸요.\n";
     public final static String ONLY_AMOUNT_TEN_MULTIPLE = "금액은 10의 단위로 입력해 주세요.\n";
+    public final static String ONLY_NUMBER = "숫자로 입력 해주세요\n";
 }
--- src/main/java/vendingmachine/util/message/ServiceMassage.java
+++ src/main/java/vendingmachine/util/message/ServiceMassage.java
@@ -1,4 +1,15 @@
 package vendingmachine.util.message;
 
 public class ServiceMassage {
+
+    public final static String INPUT_VENDINGMACINE_AMOUNT = "자판기가 보유하고 있는 금액을 입력해 주세요.\n";
+    public final static String INPUT_REGISTER_ITEMS = "상품명과 가격, 수량을 입력해 주세요.\n";
+    public final static String INPUT_USER_AMOUNT = "투입 금액을 입력해 주세요.\n";
+    public final static String INPUT_PURCHASE_ITEM_NAME = "구매할 상품명을 입력해 주세요.\n";
+
+
+    public final static String PRINT_VENDINGMACHINE_COINS = "자판기가 보유한 동전\n";
+    public final static String PRINT_USER_BALANCE = "투입 금액: %d원\n";
+
+    public final static String PRINT_CHANGE = "잔돈\n";
 }
