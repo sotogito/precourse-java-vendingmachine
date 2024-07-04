8014caba2a8c5092e102e617a78e5dd6aab1ff29
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-04 19:27:31 +0900

    feat : 자판기 금액 입력받기

--- src/main/java/vendingmachine/controller/MainController.java
+++ src/main/java/vendingmachine/controller/MainController.java
@@ -1,7 +1,28 @@
 package vendingmachine.controller;
 
+import vendingmachine.domain.VendingMachine;
+import vendingmachine.view.Input;
+import vendingmachine.view.Output;
+
 public class MainController {
     public void main(){
+        VendingMachine vendingMachine = createVendingMachine();
+        System.out.println(vendingMachine);
         
     }
+
+    private VendingMachine createVendingMachine(){
+        while (true){
+            try{
+                return new VendingMachine(Input.inputVendingMachineAmount());
+
+            }catch (IllegalArgumentException e){
+                Output.printError(e.getMessage());
+            }
+        }
+    }
+
+
+
+
 }
--- src/main/java/vendingmachine/view/Input.java
+++ src/main/java/vendingmachine/view/Input.java
@@ -1,4 +1,16 @@
 package vendingmachine.view;
+import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.util.message.ErrorMessage;
+import vendingmachine.util.message.ServiceMassage;
 
 public class Input {
+
+    public static int inputVendingMachineAmount(){
+        System.out.print(ServiceMassage.INPUT_VENDINGMACINE_AMOUNT);
+        try{
+            return Integer.parseInt(Console.readLine());
+        }catch (NumberFormatException e){
+            throw new NumberFormatException(ErrorMessage.ONLY_NUMBER);
+        }
+    }
 }
