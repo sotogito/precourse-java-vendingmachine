b10cd789653a0cd56c74e897e30be1b6c917ec90
sotogito <108535526+sotogito@users.noreply.github.com>
2024-07-04 18:56:52 +0900

    test : 상품 등록 input String parser 테스트

---
+++ src/test/java/vendingmachine/domain/ItemParserTest.java
@@ -0,0 +1,47 @@
+package vendingmachine.domain;
+
+import org.junit.jupiter.api.Test;
+import vendingmachine.domain.product.Item;
+import vendingmachine.domain.product.Items;
+
+import java.util.EnumMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ItemParserTest {
+
+
+    @Test
+    public void testParseInvalidInputNull() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ItemParser.parseItems(null);
+        });
+        assertEquals("Input string cannot be null or empty", exception.getMessage());
+    }
+
+    @Test
+    public void testParseInvalidInputEmpty() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ItemParser.parseItems("");
+        });
+        assertEquals("Input string cannot be null or empty", exception.getMessage());
+    }
+
+    @Test
+    public void testParseInvalidInputFormat() {
+        String input = "[콜라,1500,20];[사이다,1000]";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ItemParser.parseItems(input);
+        });
+        assertEquals("Each item must have a name, price, and stock", exception.getMessage());
+    }
+
+    @Test
+    public void testParseInvalidInputNonIntegerPriceStock() {
+        String input = "[콜라,1500a,20];[사이다,1000,10b]";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ItemParser.parseItems(input);
+        });
+        assertEquals("Price and stock must be integers", exception.getMessage());
+    }
+}
